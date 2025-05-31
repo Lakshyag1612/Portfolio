bddeefca8c159679049c985ddc9b1087af0279ab
Lakshya garg <lakshya.2125csme1032@kiet.edu>
2025-05-31 23:13:12 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,27 @@
 import React from "react";
 import { Link } from "react-scroll";
 import "./Navbar.css";
+import logo from "../assets/logo.png";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">JS Jigar</div>
-      <ul className="nav-links">
-        {["Home", "About", "Skills", "Education", "Work", "Experience", "Contact"].map((item) => (
-          <li key={item}>
-            <Link to={item.toLowerCase()} smooth={true} duration={500}>
-              {item}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+  <div className="wrapper">
+    <div className="logo">
+       <img src={logo} alt="Logo" className="logo-img" />
+    </div>
+    <ul className="nav-links">
+      {["Home", "About", "Skills", "Education", "Work", "Experience", "Contact"].map((item) => (
+        <li key={item}>
+          <Link to={item.toLowerCase()} smooth={true} duration={500}>
+            {item}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+</nav>
+
   );
 }
 
